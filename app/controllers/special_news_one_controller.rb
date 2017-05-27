6ee85ef8d11e245dfcc3c6bb7a9f09266919a425
ryrbsd <ryrbsd@berkeley.edu>
2017-05-27 05:45:15 +0000

    test special one

--- app/controllers/special_news_one_controller.rb
+++ app/controllers/special_news_one_controller.rb
@@ -2,6 +2,8 @@ class SpecialNewsOneController < ApplicationController
     
     def index
         @news = SpecialNewsOne.where(:Authorized => true)
+        @news_one = @news.where(:id => 1).first
+        @news_two = @news.where(:id => 2).first
     end
     
     def show
